dbc436b7452e6c16637b1d452884c115e6d57fda
Rohit-Deshbhratar <deshbhratarrohit9@gmail.com>
2023-07-30 12:35:54 +0530

    Added Excel File

--- Tests/test_Search.py
+++ Tests/test_Search.py
@@ -1,12 +1,15 @@
+import pytest
 from PageObjects.HomePage import HomePage
 from PageObjects.SearchPage import SearchPage
 from Tests.BaseTest import BaseTest
+from Utilities import ExcelUtils
 
 
 class TestSearch(BaseTest):
-    def test_search_valid_item(self):
+    @pytest.mark.parametrize("product_name", ExcelUtils.get_data_from_excel("TestData/LambdaTest.xlsx", "Products"))
+    def test_search_valid_item(self, product_name):
         home_page = HomePage(self.driver)
-        home_page.enter_product_into_search_box("HP")
+        home_page.enter_product_into_search_box(product_name)
         home_page.click_on_search_button()
         search_page = SearchPage(self.driver)
         assert search_page.display_status_of_hp_product()
